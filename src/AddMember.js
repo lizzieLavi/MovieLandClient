d9d21647ea5110f657ab0675a56848155df725a5
unknown <lizzie.olishuk1@gmail.com>
2021-10-28 15:27:05 +0300

    fix bug

--- src/AddMember.js
+++ src/AddMember.js
@@ -1,48 +1,87 @@
-import axios from 'axios';
-import {useState } from 'react';
-import {Link} from 'react-router-dom'
-import './css/editMovie.css'
-
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./css/editMovie.css";
 
 function AddMember(props) {
+  const [Name, setName] = useState("");
+  const [Email, setEmail] = useState([]);
+  const [City, setCity] = useState("");
+  const config = { headers: { "x-access-token": sessionStorage["token"] } };
 
-  const [Name,setName] = useState('')
-  const [Email,setEmail] = useState([])
-  const [City,setCity] = useState('')
-  const config ={'headers': {'x-access-token': sessionStorage["token"]}}
-
-
-  const saveMember = async() =>
-  {  
-    var newMember = {Name:Name,Email:Email,City:City}
-    await axios.post('https://cinemaws.herokuapp.com/api/Members/',newMember,config)
-    props.UpdateCallBack()
-  }
-  
+  const saveMember = async () => {
+    var newMember = { Name: Name, Email: Email, City: City };
+    await axios.post(
+      "https://cinemaws.herokuapp.com/api/Members/",
+      newMember,
+      config
+    );
+    props.UpdateCallBack();
+  };
 
   return (
-    <div className='editPage'>
+    <div className="editPage">
       <div className="border">
-        <h1 className='title'>Add Member:</h1>
-        <label className='labels' for="Name">Full Name: </label>
-        <input  className='data' id='Name' type="text"  onChange={(e)=> setName(e.target.value)} /><br/>
-        <label  className='labels' for="Email">Email: </label>
-        <input  className='data' id='Email' type="text"  onChange={(e)=> setEmail(e.target.value)} /><br/>
-        <label  className='labels' for="City">City: </label>
-        <input  className='data' id='City' type="text"  onChange={(e)=> setCity(e.target.value)} /><br/>
+        <h1 className="title">Add Member:</h1>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <label className="labels" for="Name">
+            Full Name:{" "}
+          </label>
+          <input
+            required
+            className="data"
+            id="Name"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <label className="labels" for="Email">
+            Email:{" "}
+          </label>
+          <input
+            required
+            className="data"
+            id="Email"
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <label className="labels" for="City">
+            City:{" "}
+          </label>
+          <input
+            required
+            className="data"
+            id="City"
+            type="text"
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <br />
 
-        <div>
-          <button onClick={saveMember} > <Link  className='link' to={`/Subscriptions`}>Save</Link></button>
-          <button > <Link  className='cardsLinks' to={`/Subscriptions`}>Cancel </Link></button>
-        </div>
-        
+          <div>
+            <button type="submit" onClick={saveMember}>
+              {" "}
+              <Link className="link" to={`/Subscriptions`}>
+                Save
+              </Link>
+            </button>
+            <button>
+              {" "}
+              <Link className="cardsLinks" to={`/Subscriptions`}>
+                Cancel{" "}
+              </Link>
+            </button>
+          </div>
+        </form>
       </div>
-
     </div>
   );
-
 }
 
-
 export default AddMember;
-
--- src/AddUser.js
+++ src/AddUser.js
@@ -1,94 +1,180 @@
-import axios from 'axios';
-import {useState } from 'react';
-import {Link} from 'react-router-dom'
-import './css/editAndAddUser.css'
-
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./css/editAndAddUser.css";
 
 function AddUser(props) {
+  const [FirstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState([]);
+  const [SessionTimeOut, setSessionTimeOut] = useState("");
+  const [UserName, setUserName] = useState("");
+  const [Password, setPassword] = useState(0);
+  const [checkedPermissions, setCheckedPermissions] = useState([
+    { name: "View Subscription", checked: false },
+    { name: "Create Subscription", checked: false },
+    { name: "Delete Subscription", checked: false },
+    { name: "View Movie", checked: false },
+    { name: "Create Movie", checked: false },
+    { name: "Delete Movie", checked: false },
+  ]);
+  const config = { headers: { "x-access-token": sessionStorage["token"] } };
 
-  const [FirstName,setFirstName] = useState('')
-  const [LastName,setLastName] = useState([])
-  const [SessionTimeOut,setSessionTimeOut] = useState('')
-  const [UserName,setUserName]=useState('')
-  const [Password,setPassword] = useState(0)
-  const [checkedPermissions, setCheckedPermissions] = useState([{name:"View Subscription" ,checked:false},{name: "Create Subscription",checked:false},
-  {name:"Delete Subscription",checked:false},{name: "View Movie", checked: false} ,
-  {name:"Create Movie" , checked:false},{name: "Delete Movie",checked: false}]);
-  const config ={'headers': {'x-access-token': sessionStorage["token"]}}
+  const saveUser = async () => {
+    var dateInMil = Date.now();
+    var date = new Date(dateInMil);
+    var newLogin = { username: UserName, password: Password };
+    var returnedLoginData = await axios.post(
+      "https://cinemaws.herokuapp.com/api/LogIn/",
+      newLogin,
+      config
+    );
+    var newUser = {
+      id: returnedLoginData.data.userId,
+      FirstName: FirstName,
+      LastName: LastName,
+      CreatedDate: date.toLocaleDateString(),
+      SessionTimeOut: SessionTimeOut,
+    };
+    await axios.post(
+      "https://cinemaws.herokuapp.com/api/users/",
+      newUser,
+      config
+    );
+    var permissionsArr = [];
 
-  const saveUser = async() =>
-  {  
-    var dateInMil= Date.now()
-    var date=new Date(dateInMil)
-    var newLogin = {username:UserName,password:Password}
-    var returnedLoginData= await axios.post('https://cinemaws.herokuapp.com/api/LogIn/',newLogin,config)
-    var newUser={id:returnedLoginData.data.userId, FirstName:FirstName,LastName:LastName,CreatedDate:date.toLocaleDateString(),SessionTimeOut:SessionTimeOut}
-    await axios.post('https://cinemaws.herokuapp.com/api/users/',newUser,config)
-    var permissionsArr = []
+    checkedPermissions.map((permission, index) => {
+      if (permission.checked === true)
+        permissionsArr = [...permissionsArr, permission.name];
+    });
 
-    checkedPermissions.map((permission,index)=>
-    {
-      if(permission.checked===true)
-        permissionsArr=[...permissionsArr,permission.name] 
-             
-    })
+    await axios.post(
+      "https://cinemaws.herokuapp.com/api/Permissions",
+      { id: returnedLoginData.data.userId, permissions: permissionsArr },
+      config
+    );
 
-    await axios.post('https://cinemaws.herokuapp.com/api/Permissions',{id:returnedLoginData.data.userId,permissions:permissionsArr},config)
-    
-    props.UpdateCallBack()
+    props.UpdateCallBack();
+  };
 
-  }
+  const checkPermission = (e) => {
+    var PerArr = checkedPermissions;
+    PerArr.map((permission, index) => {
+      if (permission.name === e.target.value)
+        permission.checked = !permission.checked;
+    });
 
-  const checkPermission= (e) =>
-  {
-    var PerArr=checkedPermissions
-    PerArr.map((permission,index)=>
-    {
-      if(permission.name === e.target.value)
-        permission.checked= (!permission.checked)  
-    })
+    setCheckedPermissions(PerArr);
+  };
 
-    setCheckedPermissions(PerArr)
-  }
-  
   return (
-    <div className='editUser'>
+    <div className="editUser">
       <div className="border">
-        <div className='userData'>
-          <h1 className='title'>Add User:</h1>
-          <label className='labels' for="firstName">First Name: </label>
-          <input className='data' id='firstName' type="text"  onChange={(e)=> setFirstName(e.target.value)} /><br/>
-          <label  className='labels' for="lastName">Last Name: </label>
-          <input className='data' id='lastName' type="text"  onChange={(e)=> setLastName(e.target.value)} /><br/>
-          <label  className='labels' for="userName">User Name: </label>
-          <input className='data' id='userName' type="text"  onChange={(e)=> setUserName(e.target.value)} /><br/>
-          <label  className='labels' for="Password">Password: </label>
-          <input className='data' id='Password' type="text"  onChange={(e)=> setPassword(e.target.value)} /><br/>
-          <label className='labels' for="sessionTimeOut">Session Time Out </label>
-          <input className='data' id='genres' type="text" onChange={(e)=>setSessionTimeOut(e.target.value)} /><br/>
-        </div>
+        <form>
+          <div className="userData">
+            <h1 className="title">Add User:</h1>
+            <label className="labels" for="firstName">
+              First Name:{" "}
+            </label>
+            <input
+              className="data"
+              id="firstName"
+              type="text"
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+            <br />
+            <label className="labels" for="lastName">
+              Last Name:{" "}
+            </label>
+            <input
+              className="data"
+              id="lastName"
+              type="text"
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+            <br />
+            <label className="labels" for="userName">
+              User Name:{" "}
+            </label>
+            <input
+              className="data"
+              id="userName"
+              type="text"
+              onChange={(e) => setUserName(e.target.value)}
+              required
+            />
+            <br />
+            <label className="labels" for="Password">
+              Password:{" "}
+            </label>
+            <input
+              className="data"
+              id="Password"
+              type="text"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <br />
+            <label className="labels" for="sessionTimeOut">
+              Session Time Out{" "}
+            </label>
+            <input
+              className="data"
+              id="genres"
+              type="text"
+              onChange={(e) => setSessionTimeOut(e.target.value)}
+              required
+            />
+            <br />
+          </div>
 
-        <label className='permissionsLable' id='Permissions' type='text'>Permissions:</label>
+          <label
+            className="permissionsLable"
+            id="Permissions"
+            type="text"
+            style={{ justifyContent: "center" }}
+          >
+            Permissions:
+          </label>
 
-        <div className='PemissionsStyle'>
-           {checkedPermissions.map((permission,index) => {
-            return (
-             <div className='checkboxItem'>
-               <input  id={index} className='perCheckbox' onClick={checkPermission} type="checkbox"  value={permission.name} /> 
-               <label className='checkLabel' for={index}>{permission.name}</label>
-             </div> )
-           })}
-       </div>
-       
-       <div>
-          <button onClick={saveUser} > <Link  className='cardsLinks' to={`/Users`}>save</Link></button>
-          <button > <Link  className='cardsLinks' to={`/Users`}>cancel </Link></button>
-       </div>
+          <div className="PemissionsStyle">
+            {checkedPermissions.map((permission, index) => {
+              return (
+                <div className="checkboxItem">
+                  <input
+                    id={index}
+                    className="perCheckbox"
+                    onClick={checkPermission}
+                    type="checkbox"
+                    value={permission.name}
+                  />
+                  <label className="checkLabel" for={index}>
+                    {permission.name}
+                  </label>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="form_buttons">
+            <button type="submit" onClick={saveUser}>
+              {" "}
+              <Link className="cardsLinks" to={`/Users`}>
+                save
+              </Link>
+            </button>
+            <button>
+              {" "}
+              <Link className="cardsLinks" to={`/Users`}>
+                cancel{" "}
+              </Link>
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
 }
 
-
 export default AddUser;
-
